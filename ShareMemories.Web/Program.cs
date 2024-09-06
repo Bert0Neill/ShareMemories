5afccacceb936ba46e573302836124dce3b5296c
BertONeill-V1 <bert.oneill@version1.com>
2024-09-06 10:38:54 +0100

    adding blazor licence and packages 23.x.x

--- ShareMemories.Web/Program.cs
+++ ShareMemories.Web/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ShareMemories.Web;
+using Syncfusion.Blazor;
+
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NGaF5cXmdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWXZedXRXQ2NdUER0W0Y=");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); //https://localhost:7273
 
 builder.Services.AddOidcAuthentication(options =>
 {
@@ -14,5 +18,8 @@ builder.Services.AddOidcAuthentication(options =>
     // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
+
+
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
